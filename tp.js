a000cf56760b3b0350e5dc999adb2d403caca7fe
Leonardo Sainz <leo.database92@gmail.com>
2021-04-01 17:18:48 -1200

    primer ejercicio completado

--- tp.js
+++ tp.js
@@ -1,10 +1,20 @@
 /* 
-Realizar una función que permita contar la cantidad de vocales que tiene un texto que se recibe como parámetro. No habrá discriminación entre las vocales en mayúscula y en minúscula. Las vocales acentuadas no se contarán. El valor obtenido se retornará al terminar la función. Si se no se recibe un string retornará -1.
+Realizar una función que permita contar la cantidad de vocales que tiene un texto que se recibe como parámetro. No habrá discriminación entre las vocales en mayúscula y en minúscula. Las vocales acentuadas no se contarán. El valor obtenido se retornará al terminar la función. Si no se recibe un string retornará -1.
 */
-function contarVocales(texto) {
-  return
-}
 
+function contarVocales(texto) {
+  let contador=0;
+if(typeof(texto) == "string"){
+for(let vocal of texto.toLowerCase()){
+  if(vocal == "a" || vocal == "e" || vocal == "i" || vocal == "o" || vocal == "u" ){
+  contador+=1
+    }
+   }
+  }else {
+  contador--
+}
+return contador
+}
 /* 
 Crear un repo en github y subir todo el proyecto. Se ignorará la carpeta node_modules (para ellos está creado el archivo .gitignore en este proyecto) Esta función devolverá un string con la url del repo.
 */
